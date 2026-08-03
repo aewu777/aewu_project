03e7aa052816229f0d6a3114f8c0462d2d12c61a
aewu777 <xardmax2000@gmail.com>
2026-08-03 17:31:22 +0700

    footer text to gray color

--- aewu_project/components/footer.py
+++ aewu_project/components/footer.py
@@ -6,9 +6,9 @@ from ..config import PROJECT_LINKS
 def footer() -> rx.Component:
     return rx.hstack(
         rx.hstack(
-            rx.text("2026"),
-            rx.icon("copyright", size=16),
-            rx.text("by aewu with love."),
+            rx.text("2026", color_scheme='gray'),
+            rx.icon("copyright", color='gray', size=16),
+            rx.text("by aewu with love.", color_scheme='gray'),
             align="center"
         ),
         rx.hstack(
